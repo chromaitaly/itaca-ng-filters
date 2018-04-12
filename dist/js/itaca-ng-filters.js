bc3c8226ab0a8bfe6102a4f0ce40a60a46cce3af
scoletti <simone.coletti@chromaitaly.com>
2018-04-12 12:48:39 +0200

    agggiornati dist

--- dist/js/itaca-ng-filters.js
+++ dist/js/itaca-ng-filters.js
@@ -36,6 +36,41 @@
 
 (function() {
     "use strict";
+    AmountFilter.$inject = [ "$sce", "chCurrencyFilter" ];
+    angular.module("itaca.filters").filter("chAmount", AmountFilter);
+    function AmountFilter($sce, chCurrencyFilter) {
+        return function(amount, showDiscount, hideZero) {
+            var text = null;
+            if (amount) {
+                text = "<span>";
+                var type = amount.type || "PRICE";
+                var finalAmount = _.isNil(amount.finalAmount) ? amount.initialAmount : amount.finalAmount;
+                if (!finalAmount && hideZero) {
+                    return null;
+                }
+                finalAmount = finalAmount || 0;
+                if (showDiscount && amount.initialAmount) {
+                    if (type == "PRICE" && amount.initialAmount > finalAmount) {
+                        text += '<small class="text-gray-light text-striked">' + chCurrencyFilter(amount.initialAmount) + "</small>&nbsp;";
+                    } else if (type == "PERCENTAGE" && amount.initialAmount < finalAmount) {
+                        text += '<small class="text-gray-light text-striked">' + amount.initialAmount + "%</small>&nbsp;";
+                    }
+                }
+                text += "<span>";
+                if (type == "PRICE") {
+                    text += chCurrencyFilter(finalAmount);
+                } else if (type == "PERCENTAGE") {
+                    text += finalAmount + "%";
+                }
+                text += "</span></span>";
+            }
+            return $sce.trustAsHtml(text);
+        };
+    }
+})();
+
+(function() {
+    "use strict";
     BeautifyCardFilter.$inject = [ "ReservationUtils" ];
     angular.module("itaca.filters").filter("beautifyCard", BeautifyCardFilter);
     function BeautifyCardFilter(ReservationUtils) {
