8f86fe4c4bb8ea572e78d37eb2c6fe3f6f1092bf
paolo <paolo@pmazzotta>
2018-09-19 12:14:02 +0200

    adeguate modules

--- dist/js/itaca-ng-filters.js
+++ dist/js/itaca-ng-filters.js
@@ -2,7 +2,7 @@
 ********************************************************************************
 ********************************************************************************
 ***	   itaca-ng-filters														 
-***    Copyright (C) 2016   Chroma Italy Hotels srl	 
+***    Copyright (C) 2016-2018   Chroma Italy Hotels srl	 
 ***                                                                          
 ***    This program is free software: you can redistribute it and/or modify  
 ***    it under the terms of the GNU General Public License as published by  
@@ -285,6 +285,67 @@
     function UtcDateFilter(dateFilter) {
         return function(date, format) {
             return dateFilter(date, format, "UTC");
+        };
+    }
+})();
+
+(function() {
+    "use strict";
+    UtcTimeAgoFilter.$inject = [ "amTimeAgoFilter" ];
+    angular.module("itaca.filters").filter("utcTimeAgo", UtcTimeAgoFilter);
+    function UtcTimeAgoFilter(amTimeAgoFilter) {
+        return function(value, suffix) {
+            return amTimeAgoFilter(moment(value).utcOffset(0, true), suffix);
+        };
+    }
+})();
+
+(function() {
+    "use strict";
+    ValueFilter.$inject = [ "$sce", "$translate", "chCurrencyFilter" ];
+    angular.module("itaca.filters").filter("chValue", ValueFilter);
+    function ValueFilter($sce, $translate, chCurrencyFilter) {
+        return function(value) {
+            var text = null, translate = null;
+            if (value && value.unit) {
+                value.count = value.count || 0;
+                translate = $translate.instant([ "date.hour", "date.hours", "date.day", "date.days", "common.night", "common.nights", "date.week", "date.weeks", "date.month", "date.months", "date.year", "date.years", "common.unit", "common.units" ]);
+                text = value.count + " ";
+                switch (value.unit) {
+                  case "CURRENCY":
+                    text = chCurrencyFilter(value.count);
+                    break;
+
+                  case "PERCENTAGE":
+                    text = value.count + "%";
+                    break;
+
+                  case "HOURS":
+                    text += value.count == 1 ? translate["date.hour"] : translate["date.hours"];
+                    break;
+
+                  case "DAYS":
+                    text += value.count == 1 ? translate["date.day"] : translate["date.days"];
+                    break;
+
+                  case "NIGHTS":
+                    text += value.count == 1 ? translate["common.night"] : translate["common.nights"];
+                    break;
+
+                  case "WEEKS":
+                    text += value.count == 1 ? translate["date.week"] : translate["date.weeks"];
+                    break;
+
+                  case "MONTHS":
+                    text += value.count == 1 ? translate["date.month"] : translate["date.months"];
+                    break;
+
+                  case "YEARS":
+                    text += value.count == 1 ? translate["date.year"] : translate["date.years"];
+                    break;
+                }
+                return $sce.trustAsHtml(text);
+            }
         };
     }
 })();
