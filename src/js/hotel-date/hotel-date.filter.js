b5e6d11a534544d2c0032a23c191d4817552f266
paolo <paolo@pmazzotta>
2019-05-22 18:58:33 +0200

    fix date filter

--- src/js/hotel-date/hotel-date.filter.js
+++ src/js/hotel-date/hotel-date.filter.js
@@ -5,21 +5,16 @@
 	
 	/* @ngInject */
 	function HotelDateFilter(offsetDateFilter, DateUtils, AppOptions) {
-		return function(date, format, offsetSeconds) {
-			if (offsetSeconds) {
-				offsetSeconds = Number(offsetSeconds);
-			
-			} else {
+		return function(date, format, timeZoneId) {
+			if (!timeZoneId) {
 				if (AppOptions.hotel && AppOptions.hotel.addressInfo && AppOptions.hotel.addressInfo.timeZoneId) {
-					var tz = moment.tz.zone(AppOptions.hotel.addressInfo.timeZoneId);
-					offsetSeconds = tz ? tz.offset(Date.now()) * 60 : AppOptions.defaultOffset;
-
-				} else {
-					offsetSeconds = AppOptions.defaultOffset;
+					timeZoneId = AppOptions.hotel.addressInfo.timeZoneId;
 				}
 			}
 			
-			return offsetDateFilter(date, format, DateUtils.secondsToOffsetString(offsetSeconds));
+			var tz = moment.tz.zone(timeZoneId);
+			
+			return offsetDateFilter(date, format, DateUtils.secondsToOffsetString(tz ? tz.utcOffset(Date.now()) * 60 : AppOptions.defaultOffset));	
 		};
 	}
 })();
